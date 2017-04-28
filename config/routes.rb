0b8091296645da9f2d3fb31b99b38c73134dc87d
Vaibhav Singhal <vsinghal85@gmail.com>
2017-04-28 08:12:30 -0400

    Devise and full calender and event scheduling setup

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :teachers
+  devise_for :clients
+  resources :events
+  root 'visitors#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
